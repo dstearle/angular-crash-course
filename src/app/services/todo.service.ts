e8766d11cca2488ca2fa8817678f220ac2490623
dstearle <dallas.st.earle@gmail.com>
2020-04-13 12:29:36 -0700

    Updated todo.services to use a todo list from jsonplaceholder

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,36 +1,45 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  todosUrl: 'https://jsonplaceholder.typicode.com/todos';
+
   constructor(private http:HttpClient) { }
 
   // The list of todo objects
-  getTodos() {
+  getTodos():Observable<Todo[]> {
 
+    // Replaced by Http request
     // Array of todos
-    return [
+    // return [
 
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      },
+    //   {
+    //     id: 1,
+    //     title: 'Todo One',
+    //     completed: false
+    //   },
+    //   {
+    //     id: 2,
+    //     title: 'Todo Two',
+    //     completed: true
+    //   },
+    //   {
+    //     id: 3,
+    //     title: 'Todo Three',
+    //     completed: false
+    //   },
 
-    ]
+    // ]
+
+    return this.http.get<Todo[]>(this.todosUrl);
 
   }
 
